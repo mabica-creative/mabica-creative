cd29c1ed2a54902f09d6ed3818c2be52f9b1ace2
Osi ic <osiic.offcl@gmail.com>
2025-07-10 17:11:16 +0700

    feat: nambahin navbar join mabica ke /join-mabica

--- src/lib/data/dataNavbar.ts
+++ src/lib/data/dataNavbar.ts
@@ -5,6 +5,7 @@ interface links {
 
 const dataNavbar: links[] = [
   { title: "Home", href: "/#" },
+  { title: "Join Mabica", href: "/join-mabica" },
   { title: "About Us", href: "/#about" },
   { title: "Previews", href: "/#audiobooks" },
   { title: "Audiobooks", href: "/audiobooks" },
